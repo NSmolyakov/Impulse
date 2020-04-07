d465a3353789814e37fdd189cdcb760827fdc672
NSmolyakov <45562598+NSmolyakov@users.noreply.github.com>
2020-04-07 19:12:06 +0700

    add react-router-dom, create Router and Switch

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import LogIn from './components/LogIn/LogIn';
+import {BrowserRouter,Switch,Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-       <LogIn />
+      <BrowserRouter>
+        <Switch>
+
+          <Route path="/">
+            <LogIn />
+          </Route>
+
+        </Switch>
+
+      </BrowserRouter>
+       
     </div>
   );
 }
