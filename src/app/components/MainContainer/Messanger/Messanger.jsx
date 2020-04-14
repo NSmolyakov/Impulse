0f195c1d02a211798ef4a54a106b2feeac691e03
NSmolyakov <45562598+NSmolyakov@users.noreply.github.com>
2020-04-14 20:35:30 +0700

    add emptyChecker func and fix input styles

--- src/app/components/MainContainer/Messanger/Messanger.jsx
+++ src/app/components/MainContainer/Messanger/Messanger.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import EasyLogo from '../../../assets/img/easyLogo.svg'
 import Send from '../../../assets/img/send.svg'
-
+import Message from './Message/Message'
 import './Messanger.css'
 
-function Messanger () {
-    return(
+
+
+function Messanger() {
+
+    let emptyCheker = true;
+
+    return (
         <div className='Messanger'>
             <div className="MessangerWrapper">
-                
-                 <div className='EmptyMessanger'>
-                     <img src={EasyLogo} alt='no message' />
-                        <p>Пока нет сообщений.<br/>
-                        Напишите что-нибудь</p>
-                </div>
 
-           
+                { emptyCheker   ?   <div className='EmptyMessanger'>
+                                        <img src={EasyLogo} alt='no message' />
+                                        <p>Пока нет сообщений.<br />
+                                        Напишите что-нибудь</p>
+                                    </div>
+
+                                : <Message />
+                }
 
                 <div className='sendMessage'>
-                     <input type="text" placeholder="Введите своё сообщение..."></input>
-                     <img src={Send} alt='send' />
+                    <input type="text" placeholder="Введите своё сообщение..."></input>
+                    <img src={Send} alt='send' />
                 </div>
-                </div>
-      
+            </div>
+
 
         </div>
-       
+
     )
 }
 
